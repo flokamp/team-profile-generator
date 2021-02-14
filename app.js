179b7dbddf763d918da6c8491c4f44934a653625
flokamp <74511633+flokamp@users.noreply.github.com>
2021-02-14 13:32:34 -0600

    function to get team name

--- app.js
+++ app.js
@@ -5,62 +5,28 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-// Function to initalize prompts
-const init = () => {
-  inquirer.prompt([
-    {
+let teamArr = [];
+
+// Function to get tean name
+function getTeamName() {
+  inquirer
+    .prompt({
       type: 'input',
-      name: 'manager-name',
-      message: 'What is your managers name? (Required)',
+      name: 'teamname',
+      message: 'What is your team name?',
       validate: nameInput => {
         if (nameInput) {
           return true;
         } else {
-          console.log('Please enter your managers name!');
+          console.log('Please enter your team name!');
           return false;
         }
       }
-    },
-    {
-      type: 'input',
-      name: 'manager-id',
-      message: 'What is your managers ID? (Required)',
-      validate: idInput => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log('Please enter your managers ID!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'manager-email',
-      message: 'What is your managers email? (Required)',
-      validate: emailInput => {
-        if (emailInput) {
-          return true;
-        } else {
-          console.log('Please enter your managers email!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'number',
-      name: 'office-number',
-      message: 'What is your managers office number? (Required)',
-      validate: officeNumberInput => {
-        if (officeNumberInput) {
-          return true;
-        } else {
-          console.log('Please enter your managers office number!');
-          return false;
-        }
-      }
-    },
-  ])
+    }).then(data => {
+      const teamName = data.teamname;
+      teamArr.push(teamName);
+      console.log(teamArr)
+    })
 }
 
-init()
+getTeamName();
