045a5292019ddb34950863acf84666e261d940e1
flokamp <74511633+flokamp@users.noreply.github.com>
2021-02-14 13:44:40 -0600

    prompts to get manager details

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const Intern = require('./lib/Intern');
 
 let teamArr = [];
 
-// Function to get tean name
-function getTeamName() {
+// Function to get team name
+function teamName() {
   inquirer
     .prompt({
       type: 'input',
@@ -26,7 +26,77 @@ function getTeamName() {
       const teamName = data.teamname;
       teamArr.push(teamName);
       console.log(teamArr)
+      managerDetails()
     })
 }
 
-getTeamName();
+
+// Function to get manager info
+function managerDetails() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your managers name?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your managers name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'number',
+        name: 'id',
+        message: 'What is your managers ID number?',
+        validate: idInput => {
+          if (idInput === NaN) {
+            console.log('Please enter your managers ID number!');
+            return false;
+          } else {
+            return true;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your managers email?',
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your managers email!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'number',
+        name: 'officeNumber',
+        message: 'What is your managers office number?',
+        validate: officeNumberInput => {
+          if (officeNumberInput) {
+            return true;
+          } else {
+            console.log('Please enter your managers office number!');
+            return false;
+          }
+        }
+      },
+    ]).then(data => {
+      const name = data.name;
+      const id = data.id;
+      const email = data.email;
+      const officeNumber = data.officeNumber;
+      const manager = new Manager(name, id, email, officeNumber)
+      teamArr.push(manager);
+      console.log(teamArr)
+      teamMemberDetails()
+    })
+}
+
+teamName();
