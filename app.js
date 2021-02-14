eada8edbff2c29540e5ddc7f2c8af8a99cc35dd3
flokamp <74511633+flokamp@users.noreply.github.com>
2021-02-14 14:08:42 -0600

    function to collect all team member details

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -95,8 +94,163 @@ function managerDetails() {
       const manager = new Manager(name, id, email, officeNumber)
       teamArr.push(manager);
       console.log(teamArr)
-      teamMemberDetails()
+      teamMembers()
     })
+}
+
+// Function to add team members
+function teamMembers() {
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'member',
+      message: 'Would you like to add a new team member?',
+      choices: ["Yes I would like to add an engineer.", "Yes, I would like to add an intern.", "No, I've added everyone on my team."]
+    }).then(data => {
+      if (data.member === "Yes I would like to add an engineer.") {
+        addEngineer()
+      } else if (data.member === "Yes, I would like to add an intern.") {
+        addIntern()
+      } else {
+        generatePage()
+      }
+    })
+}
+
+// Function to get engineer details
+function addEngineer() {
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your engineers name?',
+      validate: answer => {
+        if (answer) {
+          return true;
+        } else {
+          console.log('Please enter your engineers name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'What is your engineers ID number?',
+      validate: answer => {
+        if (answer === NaN) {
+          console.log('Please enter your engineers ID number!');
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your engineers email?',
+      validate: answer => {
+        if (answer) {
+          return true;
+        } else {
+          console.log('Please enter your managers email!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your engineers GitHub username?',
+      validate: answer => {
+        if (answer) {
+          return true;
+        } else {
+          console.log('Please enter your engineers GitHub username!');
+          return false;
+        }
+      }
+    },
+  ]).then(data => {
+    const name = data.name;
+    const id = data.id;
+    const email = data.email;
+    const github = data.github;
+    const engineer = new Engineer(name, id, email, github)
+    teamArr.push(engineer);
+    console.log(teamArr)
+    teamMembers()
+  })
+}
+
+// Function to get intern details
+function addIntern() {
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your interns name?',
+      validate: answer => {
+        if (answer) {
+          return true;
+        } else {
+          console.log('Please enter your interns name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'What is your interns ID number?',
+      validate: answer => {
+        if (answer === NaN) {
+          console.log('Please enter your interns ID number!');
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your interns email?',
+      validate: answer => {
+        if (answer) {
+          return true;
+        } else {
+          console.log('Please enter your managers email!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What is your interns school?',
+      validate: answer => {
+        if (answer) {
+          return true;
+        } else {
+          console.log('Please enter your interns school!');
+          return false;
+        }
+      }
+    },
+  ]).then(data => {
+    const name = data.name;
+    const id = data.id;
+    const email = data.email;
+    const school = data.school;
+    const intern = new Intern(name, id, email, school)
+    teamArr.push(intern);
+    console.log(teamArr)
+    teamMembers()
+  })
 }
 
 teamName();
